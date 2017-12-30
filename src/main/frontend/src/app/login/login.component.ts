b32d0dc18c347f6100e5911ea22c312acc293183
santhikumar43 <santhi.kumar@openprisetech.com>
2017-12-30 12:00:43 +0530

    adding loading and animations

--- src/main/frontend/src/app/login/login.component.ts
+++ src/main/frontend/src/app/login/login.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import {Router} from '@angular/router';
 import {MatSnackBar} from '@angular/material';
+import { moveInLeft } from '../router.animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [moveInLeft()]
 })
 export class LoginComponent implements OnInit {
 
--- src/main/frontend/src/app/registeration/registeration.component.ts
+++ src/main/frontend/src/app/registeration/registeration.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { moveIn, fallIn } from '../router.animations';
 
 @Component({
   selector: 'app-registeration',
   templateUrl: './registeration.component.html',
-  styleUrls: ['../login/login.component.css']
+  styleUrls: ['../login/login.component.css'],
+  animations: [moveIn()]
 })
 export class RegisterationComponent implements OnInit {
 
